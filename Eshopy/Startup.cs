2b1089510a9c127f70e1d25b07e980c81cc96478
sakib717 <sakibsabbir7@gmail.com>
2016-08-17 19:21:06 -0400

    Add project files.

---
+++ Eshopy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Eshopy.Startup))]
+namespace Eshopy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
